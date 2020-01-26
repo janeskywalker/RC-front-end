48574baa44f7627925dd20fd8bdd782f444a3306
Jane Skywalker <janehuizeng@gmail.com>
2020-01-25 17:42:45 -0800

    setting up basic header

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,8 +2,14 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <header className="hero">I am Hero!!!!
-    
+    <header className="hero">
+      <div>
+        <p>RoosterGrin</p>
+      </div>
+      <h1 className='greeting'>
+        WELCOME TO Rooster Grin
+      </h1>
+      <button className='button'>Button Button</button>
     </header>
   );
 };

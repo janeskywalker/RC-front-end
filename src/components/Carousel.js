fce91bcdfdb7d56c4c2b3e745925e964444ac768
Jane Skywalker <janehuizeng@gmail.com>
2020-01-27 10:46:40 -0800

    styling carousel arrows

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -8,21 +8,21 @@ const Carousel = () => {
       {
         title: "Title One",
         ratings: 1,
-        author: "Author 1",
+        author: "Joe Doe",
         date: "01/01/20",
-        body: "Fugiat eiusmod deserunt Lorem velit duis aute ullamco officia cillum nostrud do. Magna amet culpa Lorem sunt elit dolore mollit. Aliqua fugiat Lorem voluptate veniam veniam irure id eu laborum labore quis velit culpa."
+        body: "Fugiat eiusmod deserunt Lorem velit duis aute ullamco officia cillum nostrud do. Magna amet culpa Lorem sunt elit dolore mollit. Aliqua fugiat Lorem voluptate veniam veniam irure id eu ."
       },
       {
         title: "Title Two",
         ratings: 5,
-        author: "Author 2",
+        author: "John Smith",
         date: "01/01/20",
         body: "Officia magna est in officia fugiat eiusmod pariatur amet. Exercitation incididunt proident est ipsum. Lorem deserunt cillum deserunt veniam. Exercitation tempor culpa excepteur officia. Officia officia culpa aute.."
       },
       {
         title: "Title Three",
         ratings: 3,
-        author: "Author 3",
+        author: "Elsa Jones",
         date: "01/01/20",
         body: "Sit labore proident in aliqua nisi elit dolor occaecat incididunt sit id. Sunt ad enim tempor esse esse nisi sunt tempor do exercitation ipsum incididunt est. Et eiusmod do proident aute ullamco."
       },
@@ -40,24 +40,32 @@ const Carousel = () => {
         <Rating ratings={currentSlide.ratings}/>
       </div>
       <div className="body-container">
-        <button onClick={() => {
+        {/* <button className='backward-btn' > */}
+            <div className="backward-arrow" onClick={() => {
+            if(state.currentIndex > 0){
+              setState({
+                ...state,
+                currentIndex: state.currentIndex - 1
+              })
+            }
+          }}></div>
+          {/* </button> */}
+        <div className="body-text-content">
+          <p className='author-date'>{currentSlide.author} {currentSlide.date}</p>
+          <p className="body-content">{currentSlide.body}</p>
+          <PageIndicator currentIndex={state.currentIndex} numOfSlides={numOfSlides}/>
+        </div>
+        {/* <button className='forward-btn'> */}
+          <div className="forward-arrow" onClick={() => {
           if(state.currentIndex < numOfSlides - 1) {
             setState({
               ...state,
               currentIndex: state.currentIndex + 1
             });
           }
-        }}>Go Forward</button>
-        <p className="body-content">{currentSlide.body}</p>
-        <PageIndicator currentIndex={state.currentIndex} numOfSlides={numOfSlides}/>
-        <button onClick={() => {
-          if(state.currentIndex > 0){
-            setState({
-              ...state,
-              currentIndex: state.currentIndex - 1
-            })
-          }
-        }}>Go Back</button>
+        }}></div>
+          {/* </button> */}
+
       </div>
     </section>
   );

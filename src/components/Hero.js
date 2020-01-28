81ba548be05f477f93027741ef662095a7adb73b
Jane Skywalker <janehuizeng@gmail.com>
2020-01-28 10:29:17 -0800

    centering greetings

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,16 +4,22 @@ const Hero = () => {
   return (
     <header className="hero">
       <div className="logo-contrainer">
-        <img
-          className="logo"
-          src="/assets/logo-white.svg"
-          alt="logo"
-        />
+          <img
+            className="logo"
+            src="/assets/logo-white.svg"
+            alt="logo"
+          />
       </div>
-      <div className='greetings'>
-        <h1 className="greetings-text">WELCOME TO Rooster Grin</h1>
+
+      <div className="hero-wrapper">
+        <div className='filling'></div>
+        <div className='greetings'>
+          <h1 className="greetings-text">WELCOME TO</h1>
+          <h1 className="greetings-text-2">Rooster Grin</h1>
+          <button className="btn hero-">Button Button</button>
+        </div>
+        <div className='filling'></div>
       </div>
-      <button className="btn">Button Button</button>
     </header>
   );
 };
